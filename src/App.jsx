e470c41108d18c00b87ade54671f46c3cdaa644e
Sumin Kim <zzmzzm0065@gmail.com>
2024-11-05 17:41:37 +0900

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import NavBar from './components/NavBar';
+import Hero from './components/Hero';
+
+function App() {
+  return (
+    <main className='overflow-x-hidden'>
+      <NavBar />
+      <Hero />
+    </main>
+  );
+}
+
+export default App
